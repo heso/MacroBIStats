a0d26857c2a7a2187900cb8ce70a0ae7cb1aeb9c
heso6 <heso67@gmail.com>
2022-05-17 21:42:38 +0500

    init#4

--- alchemy/callbacks.py
+++ alchemy/callbacks.py
@@ -1,15 +1,17 @@
-__all__ = ["get_sells_count", "get_booking_price_and_count", "get_target_leads_count"]
+__all__ = ['get_sells_price_and_count', 'get_booking_price_and_count', 'get_target_leads_count']
 
 from .models import Deals, Leads, get_session
 from datetime import date
 from sqlalchemy import func
 
 
-def get_sells_count(date_from: date = date(2000, 1, 1),
-                    date_to: date = date.today()):
+def get_sells_price_and_count(date_from: date = date(2000, 1, 1),
+                              date_to: date = date.today()):
     with get_session().begin() as session:
-        return session.query(Deals).filter(Deals.agreement_date >= date_from,
-                                           Deals.agreement_date <= date_to).count()
+        return session.query(func.count(Deals.id),
+                             func.coalesce(func.sum(Deals.summ), 0)).\
+                       filter(Deals.agreement_date >= date_from,
+                              Deals.agreement_date <= date_to)
 
 
 def get_target_leads_count(date_from: date = date(2000, 1, 1),
@@ -25,7 +27,7 @@ def get_target_leads_count(date_from: date = date(2000, 1, 1),
 
 def get_booking_price_and_count(date_from: date = date(2000, 1, 1),
                             date_to: date = date.today(),
-                            booking_free: bool = True):
+                            booking_paid: bool = True):
 
     with get_session().begin() as session:
         return session.query(func.count(Deals.id),
@@ -33,7 +35,7 @@ def get_booking_price_and_count(date_from: date = date(2000, 1, 1),
                filter(Deals.status_modified_date >= date_from,
                       Deals.status_modified_date <= date_to,
                       Deals.status == 105,
-                      Deals.is_payed_reserve == int(booking_free)).all()
+                      Deals.is_payed_reserve == int(booking_paid)).all()
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,56 +1,42 @@
 from datetime import date, timedelta
-from alchemy import get_target_leads_count, get_sells_count, get_booking_price_and_count
-
-
-def get_today():
-    return date.today()
-
-
-def get_yesterday():
-    return date.today() - timedelta(days=1)
+from alchemy import get_target_leads_count, get_sells_price_and_count, get_booking_price_and_count
+import telebot
+import os
 
 
 def main():
+    today = date.today()
+    yesterday = date.today() - timedelta(days=1)
+
     # всего продаж
-    total_sells = get_sells_count()
-    # продажи вчера
-    yesterday_sells = get_sells_count(get_yesterday(), get_yesterday())
-    # продажи сегодня
-    today_sells = get_sells_count(get_today(), get_today())
-
+    today_sells = get_sells_price_and_count(today, today)
     # целевых заявок вчера
-    yesterday_target_leads = get_target_leads_count(get_yesterday(), get_yesterday())
+    yesterday_target_leads = get_target_leads_count(yesterday, yesterday)
     # целевых заявок сегодня
-    today_target_leads = get_target_leads_count(get_today(), get_today())
-
-    # бесплатных броней вчера
-    yesterday_booking_free_count = get_booking_price_and_count(get_yesterday(), get_yesterday(), True)
-    # платных броней вчера
-    yesterday_booking_paid_count = get_booking_price_and_count(get_yesterday(), get_yesterday(), False)
-
+    today_target_leads = get_target_leads_count(today, today)
     # бесплатных броней сегодня
-    today_booking_free_count = get_booking_price_and_count(get_today(), get_today(), True)
+    today_booking_free_count = get_booking_price_and_count(today, today, booking_paid=False)
     # платных броней сегодня
-    today_booking_paid_count = get_booking_price_and_count(get_today(), get_today(), False)
+    today_booking_paid_count = get_booking_price_and_count(today, today, booking_paid=True)
 
-    msg = (
-        f'всего продаж - {total_sells}\n'
-        f'продаж вчера - {yesterday_sells}\n'
-        f'продаж сегодня - {today_sells}\n'
-        f'целевых заявок вчера - {yesterday_target_leads}\n'
-        f'целевых заявок сегодня - {today_target_leads}\n'
-        f'бесплатных броней сегодня: кол-во - {today_booking_free_count[0][0]}, сумма - {today_booking_free_count[0][1]}\n'
-        f'бесплатных броней вчера: кол-во - {yesterday_booking_free_count[0][0]}, сумма - {yesterday_booking_free_count[0][1]}\n'
-        f'платных броней сегодня: кол-во - {today_booking_paid_count[0][0]}, сумма - {today_booking_paid_count[0][1]}\n'
-        f'платных броней вчера: кол-во - {yesterday_booking_paid_count[0][0]}, сумма - {yesterday_booking_paid_count[0][1]}\n'
+    max_len = max(len(str(today_booking_free_count[0][0])),
+                  len(str(today_sells[0][0])),
+                  len(str(today_booking_paid_count[0][0])))
+
+    booking_free_spaces = ' ' * (max_len - len(str(today_booking_free_count[0][0])))
+    booking_paid_spaces = ' ' * (max_len - len(str(today_booking_paid_count[0][0])))
+    sells_spaces = ' ' * (max_len - len(str(today_sells[0][0])))
+
+
+    msg = (f'{today}\n\n`'
+        f'Лиды:         {today_target_leads} шт.\n'
+        f'Беспл. брони: {today_booking_free_count[0][0]} шт.{booking_free_spaces}\\ {round(today_booking_free_count[0][1]/10**6,1)} млн.\n'
+        f'Плат. брони:  {today_booking_paid_count[0][0]} шт.{booking_paid_spaces}\\ {round(today_booking_paid_count[0][1]/10**6,1)} млн.\n\n'
+        f'Договоры:     {today_sells[0][0]} шт.{sells_spaces}\\ {round(today_sells[0][1]/10**6,1)} млн.\n`'
     )
 
-    print (msg)
-    # telegram = get_notifier('telegram')
-    # telegram.notify(message=msg, token=os.environ.get('telegram_token'), chat_id=os.environ.get('chat_id'))
-
-    # bot = telebot.TeleBot(os.environ.get('telegram_token'))
-    # bot.send_message(os.environ.get('chat_id'), msg)
+    bot = telebot.TeleBot(os.environ.get('telegram_token'))
+    bot.send_message(os.environ.get('chat_id'),  msg , parse_mode='Markdown')
 
 
 if __name__ == '__main__':
