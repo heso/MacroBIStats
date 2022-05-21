2946c87a473e9d56a69e886009582994e7c8d569
heso6 <heso67@gmail.com>
2022-05-22 02:11:48 +0500

    telegram polling

--- main.py
+++ main.py
@@ -1,29 +1,78 @@
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime as dt
 from alchemy import StatDeals, StatBooking, get_target_leads_count
-from telebot import TeleBot
+import telebot
 import os
+from telebot import types
+import re
+
+chat_id = os.environ.get('chat_id')
+token = os.environ.get('telegram_token')
+bot = telebot.TeleBot(token)
+regexp_pattern = r'(\d{2}\.\d{2}\.\d{4})(,\s*)(\d{2}\.\d{2}\.\d{4})'
 
 
-def main(use_telegram: bool = True):
-    today = date.today()
-    # yesterday = date.today() - timedelta(days=2)
+markup = types.ReplyKeyboardMarkup(row_width=3)
+btn_stat_today = types.KeyboardButton('статистика за сегодня')
+btn_stat_yesterday = types.KeyboardButton('статистика за вчера')
+btn_stat_period = types.KeyboardButton('статистика за период')
 
-    leads_count = get_target_leads_count(today, today)
-    today_sells = StatDeals(today, today)
-    booking_free = StatBooking(today, today, False)
-    booking_paid = StatBooking(today, today, True)
+markup.add(btn_stat_today)
+markup.add(btn_stat_yesterday)
+markup.add(btn_stat_period)
 
-    msg = (f'{today}\n\n'
-           f'`Лиды:         {leads_count} шт.\n\n'
-           f'{booking_free.get_stats()}\n\n'
-           f'{booking_paid.get_stats()}\n\n'
-           f'{today_sells.get_stats()}`')
-    if use_telegram:
-        bot = TeleBot(os.environ.get('telegram_token'))
-        bot.send_message(os.environ.get('chat_id'), msg, parse_mode='Markdown')
+bot.send_message(chat_id, "статистика", reply_markup=markup)
+
+
+def get_statistics(date_from, date_to):
+    leads_count = get_target_leads_count(date_from, date_to)
+    today_sells = StatDeals(date_from, date_to)
+    booking_free = StatBooking(date_from, date_to, False)
+    booking_paid = StatBooking(date_from, date_to, True)
+
+    if date_from == date_to:
+        interval = date_from
     else:
-        print(msg)
+        interval = f'`Период с` {date_from} `по` {date_to}'
+
+    return (f'{interval}\n\n'
+            f'`Лиды:         {leads_count} шт.\n\n'
+            f'{booking_free.get_stats()}\n\n'
+            f'{booking_paid.get_stats()}\n\n'
+            f'{today_sells.get_stats()}`')
 
 
-if __name__ == '__main__':
-    main(1==1)
+def test_statistics(message):
+    return message.text in ['статистика за сегодня',
+                       'статистика за вчера',
+                       'статистика за период']
+
+
+@bot.message_handler(func=test_statistics)
+def send_statistics(message):
+    cid = str(message.chat.id)
+    if cid == chat_id:
+        match message.text:
+            case 'статистика за сегодня':
+                date_stat = date.today()
+                text = get_statistics(date_stat, date_stat)
+            case 'статистика за вчера':
+                date_stat = date.today() - timedelta(days=1)
+                text = get_statistics(date_stat, date_stat)
+            case 'статистика за период':
+                text = '`Введите через запятую две даты в формате   дд.мм.гггг\nНапример:`\n12.09.2021, 02.10.2022'
+        bot.send_message(chat_id, text, parse_mode='Markdown')
+
+
+@bot.message_handler(regexp=regexp_pattern)
+def send_statistics(message):
+    cid = str(message.chat.id)
+    if cid == chat_id:
+        date_from = re.search(regexp_pattern, message.text).group(1)
+        date_to = re.search(regexp_pattern, message.text).group(3)
+        text = get_statistics(dt.strptime(date_from, '%d.%m.%Y').date(), dt.strptime(date_to, '%d.%m.%Y').date())
+
+        bot.send_message(chat_id, text, parse_mode='Markdown')
+
+
+bot.infinity_polling()
+
