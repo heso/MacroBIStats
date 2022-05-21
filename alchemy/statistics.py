cdacfd5fdbf1929130959b9bf913ea9f7232c730
heso6 <heso67@gmail.com>
2022-05-21 14:37:05 +0500

    refactoring

--- alchemy/statistics.py
+++ alchemy/statistics.py
@@ -3,58 +3,82 @@ __all__ = ['StatDeals', 'StatBooking']
 from dataclasses import dataclass
 from datetime import date, timedelta
 from .callbacks import get_sells_stats, get_booking_stats
+from typing import Union
+
+categories = {'all': '',
+              'flat': '- квартиры:   ',
+              'house': '- дома:       ',
+              'comm': '- земля:      ',
+              'land': '- коммерция:  ',
+              'other': '- всякое:     '}
 
 
 @dataclass
 class StatDeals:
+    """ статистика по заключенным договорам """
     date_from: date
     date_to: date
 
     def __post_init__(self):
+        self.all_count, self.all_price = get_sells_stats(self.date_from, self.date_to)[0]
         self.flat_count, self.flat_price = get_sells_stats(self.date_from, self.date_to, ['flat'])[0]
         self.house_count, self.house_price = get_sells_stats(self.date_from, self.date_to, ['house'])[0]
         self.comm_count, self.comm_price = get_sells_stats(self.date_from, self.date_to, ['comm'])[0]
         self.land_count, self.land_price = get_sells_stats(self.date_from, self.date_to, ['land'])[0]
         self.other_count, self.other_price = get_sells_stats(self.date_from, self.date_to, ['garage', 'storageroom'])[0]
 
-    @property
-    def max_len(self):
-        result = max(len(str(self.flat_count)),
-                     len(str(self.house_count)),
-                     len(str(self.comm_count)),
-                     len(str(self.land_count)),
-                     len(str(self.other_count)))
-        return result
+    def get_stats(self):
+        name_all = 'Договоры:     '
+        return get_class_stats(self, name_all)
 
 
 @dataclass
 class StatBooking:
+    """ статистика по платным и бесплатным броням """
     date_from: date
     date_to: date
     isBookingPaid: bool
 
     def __post_init__(self):
+        self.all_count, self.all_price = get_booking_stats(self.date_from, self.date_to, self.isBookingPaid)[0]
         self.flat_count, self.flat_price = get_booking_stats(self.date_from, self.date_to, self.isBookingPaid, ['flat'])[0]
         self.house_count, self.house_price = get_booking_stats(self.date_from, self.date_to, self.isBookingPaid, ['house'])[0]
         self.comm_count, self.comm_price = get_booking_stats(self.date_from,  self.date_to, self.isBookingPaid, ['comm'])[0]
         self.land_count, self.land_price = get_booking_stats(self.date_from, self.date_to, self.isBookingPaid, ['land'])[0]
         self.other_count, self.other_price = get_booking_stats(self.date_from, self.date_to, self.isBookingPaid, ['garage', 'storageroom'])[0]
 
-    @property
-    def max_len(self):
-        result = max(len(str(self.flat_count)),
-                     len(str(self.house_count)),
-                     len(str(self.comm_count)),
-                     len(str(self.land_count)),
-                     len(str(self.other_count)))
-        return result
+    def get_stats(self):
+        if self.isBookingPaid:
+            name_all = 'Плат. брони:  '
+        else:
+            name_all = 'Беспл. брони: '
+        return get_class_stats(self, name_all)
+
+
+def get_class_stats(cls: Union[StatDeals, StatBooking], name_all: str) -> str:
+    return name_all + '\n'.join(filter(None, [get_stats_for_category(cls, key) for key in categories]))
+
+
+def get_stats_for_category(cls: Union[StatDeals, StatBooking], category: str) -> str:
+    category_count = getattr(cls, f'{category}_count')
+    category_price = getattr(cls, f'{category}_price')
+    free_spaces = ' ' * (max_len(cls) - len(str(category_count)))
+    if category_count != 0 or category == 'all':
+        return f'{categories[category]}{category_count} шт.{free_spaces}\\ {category_price} млн.'
+
+
+def max_len(cls: Union[StatDeals, StatBooking]) -> int:
+    result = max(len(str(cls.all_count)),
+                 len(str(cls.flat_count)),
+                 len(str(cls.house_count)),
+                 len(str(cls.comm_count)),
+                 len(str(cls.land_count)),
+                 len(str(cls.other_count)))
+    return result
 
 
 def main():
-    yesterday = date.today() - timedelta(days=1)
-    booking_paid_statistics = StatBooking(yesterday, yesterday, False)
-
-    print(booking_paid_statistics.max_len)
+    pass
 
 
 if __name__ == '__main__':
